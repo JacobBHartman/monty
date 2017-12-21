31fecc5c7efcc67687805051b31f9ffd4ac6fac9
JacobBHartman <209@holbertonschool.com>
2017-12-21 01:08:14 +0000

    copied files to this version of monty (the one under 0x18)

---
+++ stack.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * free_stack - frees a doubly linked list (stack)
+ * @top: a pointer to a doubly linked list (stack)
+ *
+ * Return: void
+ */
+void free_stack(stack_t *top)
+{
+	stack_t *temp = top;
+
+	while (temp != NULL)
+	{
+		temp = top->next;
+		free(top);
+		top = temp;
+	}
+}
